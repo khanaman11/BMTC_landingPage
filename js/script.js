9dca0909195fab23561769bb9364f9510b48f6a5
Aman khan <amanullahkhan11595@gmail.com>
2025-05-24 13:12:50 +0530

    new commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Heading script start
+document.addEventListener('DOMContentLoaded', function () {
+    // Select all nav links
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    navLinks.forEach(link => {
+      link.addEventListener('click', function () {
+        // Remove active-link from all
+        navLinks.forEach(l => l.classList.remove('active-link'));
+
+        // Add active-link to clicked one
+        this.classList.add('active-link');
+      });
+    });
+  });
+
+ // Heading script end
